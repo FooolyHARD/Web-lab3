a100a454e18263d2b8a775bfb0c5783a1313cb84
Senechka <64082009+FooolyHARD@users.noreply.github.com>
2022-12-15 13:22:31 +0300

    Labo4ka

---
+++ src/main/java/com/senechka/lab3/beans/Service.java
@@ -0,0 +1,68 @@
+package com.senechka.lab3.beans;
+
+import com.senechka.lab3.db.HitResult;
+import com.senechka.lab3.utils.AreaCheck;
+import com.senechka.lab3.db.DataBaseController;
+import com.senechka.lab3.utils.CoordinatesValidation;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@SessionScoped
+@Named("service")
+public class Service implements Serializable {
+    private final AreaCheck areaCheck;
+    private final DataBaseController dbController;
+
+    public Service() {
+        areaCheck = new AreaCheck();
+        dbController = new DataBaseController();
+    }
+
+    public LinkedList<HitResult> getUserHits(String sessionId) {
+        if (sessionId == null) return new LinkedList<>();
+
+        List<HitResult> hits = dbController.getUserHits(sessionId);
+
+        System.out.println("Return user hits: " + hits);
+
+        return hits != null ? new LinkedList<>(hits) : new LinkedList<>();
+    }
+
+    public HitResult processRequest(String sessionId, Coordinates coordinates) {
+        if (!CoordinatesValidation.validate(coordinates)) {
+            System.out.println("Coordinates not valid");
+            return null;
+        }
+
+        boolean isHit = areaCheck.isHit(coordinates);
+        HitResult hitResult = new HitResult(sessionId, coordinates, getCurrentDate(), isHit);
+
+        System.out.println("Get result:" + hitResult);
+
+        dbController.addHitResult(hitResult);
+
+        System.out.println("WTFFF");
+
+        return hitResult;
+    }
+
+    public void clearUserHits(String sessionId) {
+        dbController.markUserHitsRemoved(sessionId);
+    }
+
+    private String getCurrentDate() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    }
+}
